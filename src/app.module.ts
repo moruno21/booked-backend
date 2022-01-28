0eac6cc92c5c2cbfd3f646ead1b03e2c7528f678
moruno21 <amoruno21@gmail.com>
2022-01-28 01:36:35 +0100

    feat: intregration with mongodb database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const url = process.env.MONGO_URL || 'mongodb:27017';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`mongodb://${url}/tasks`)],
   controllers: [AppController],
   providers: [AppService],
 })
