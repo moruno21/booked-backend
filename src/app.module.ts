a4970016858c62e4f2078d699320cfc6fd9d2589
moruno21 <amoruno21@gmail.com>
2022-01-29 15:01:28 +0100

    feat: updated general structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 const url = process.env.MONGO_URL || 'mongodb:27017';
 
 @Module({
   imports: [MongooseModule.forRoot(`mongodb://${url}/tasks`)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
