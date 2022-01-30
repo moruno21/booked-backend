79be29cba931bca74b7c85d26e9e764e9b28fbbd
moruno21 <amoruno21@gmail.com>
2022-01-30 21:01:31 +0100

    feat: updated general structure again xd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 const url = process.env.MONGO_URL || 'mongodb:27017';
 
 @Module({
   imports: [MongooseModule.forRoot(`mongodb://${url}/tasks`)],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
