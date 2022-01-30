1f06976ff7bb7116c06a104313a2d5f7e64fd40c
moruno21 <amoruno21@gmail.com>
2022-01-30 21:26:54 +0100

    feat: books files created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksController } from './books/books.controller';
+import { BooksModule } from './books/books.module';
 
 const url = process.env.MONGO_URL || 'mongodb:27017';
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb://${url}/tasks`)],
-  controllers: [AppController],
+  imports: [MongooseModule.forRoot(`mongodb://${url}/booked`), BooksModule],
+  controllers: [AppController, BooksController],
   providers: [AppService],
 })
 export class AppModule {}
