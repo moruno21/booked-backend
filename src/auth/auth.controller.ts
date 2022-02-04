65e1af24e60212fa84ce58f1959e2e6a3baae5fe
moruno21 <amoruno21@gmail.com>
2022-02-04 14:40:51 +0100

    feat: google guard moved to guards folder

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,16 +2,17 @@ import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserSchema } from 'src/users/schema/user.schema';
 import { AuthService } from './auth.service';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('google'))
+  @UseGuards(GoogleAuthGuard)
   @Get('google')
   async signInWithGoogle(@Req() req) {}
 
-  @UseGuards(AuthGuard('google'))
+  @UseGuards(GoogleAuthGuard)
   @Get('google/redirect')
   async signInWithGoogleRedirect(@Req() req, @Res() res) {
     this.authService.signInWithGoogle(req);
