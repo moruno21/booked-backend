f9074757d802227fab64eacdd2c1a9dd2a371ed8
moruno21 <amoruno21@gmail.com>
2022-02-17 21:15:56 +0100

    fix: useless imports deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksController } from './books/books.controller';
 import { BooksModule } from './books/books.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { LoansModule } from './loans/loans.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 const url = process.env.MONGO_URL || 'mongodb:27017';
 
